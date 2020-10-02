0268bc80157a6887c418249cbe37821df6d3bd77
burn-c <chopcsom@gmail.com>
2020-10-01 23:44:36 -0300

    Draft: Erro handling

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -32,23 +32,27 @@ export interface ForecastPoint {
 }
 
 export class StormGlass {
-  readonly stormGlassApiParams = 
+  readonly stormGlassAPIParams = 
   'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
 
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-    const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
-      {
-        headers: {
-          Authorization: 'fake-token',
-        },
-      }
-    );
-
-    return this.normalizeResponse(response.data);
+    try{
+      const response = await this.request.get<StormGlassForecastResponse>(
+        `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+        {
+          headers: {
+            Authorization: 'fake-token',
+          },
+        }
+      );
+  
+      return this.normalizeResponse(response.data);
+    } catch (err) {
+        throw new Error(`Unexpected error when trying to communicate to StormGlass: ${err.message}`);
+    }
   }
 
   private normalizeResponse(
